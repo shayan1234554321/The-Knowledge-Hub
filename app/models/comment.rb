f7b92c44ffbe7ac808196953b45d53ba25b60684
shayan1234554321 <kshayan091@gmail.com>
2023-06-09 15:06:18 +0500

    Linter fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_create :update_comment_counter
+  after_create :update_comment_counter
 
-    def update_comment_counter
-        post.increment!(:comments_counter)
-    end
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
-    
-    after_create :update_likes_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def update_likes_counter
-        post.increment!(:likes_counter)
-    end
+  after_create :update_likes_counter
 
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
