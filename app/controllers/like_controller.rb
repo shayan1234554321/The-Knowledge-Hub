d78d49c013d892721064a3bc7a907b6ec8f99f0a
shayan1234554321 <kshayan091@gmail.com>
2023-06-16 17:40:15 +0500

    linter fix

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,12 +1,12 @@
 class LikeController < ApplicationController
-    def create
-      @like = Like.new( author_id_id: params[:user_id], post_id_id: params[:post_id])
-      if @like.save
-        # Successful save
-        redirect_to "/users/1/posts/#{params[:post_id]}"
-      else
-        # Failed save
-        flash[:error] = "Failed to create like"
-      end
+  def create
+    @like = Like.new(author_id_id: params[:user_id], post_id_id: params[:post_id])
+    if @like.save
+      # Successful save
+      redirect_to "/users/1/posts/#{params[:post_id]}"
+    else
+      # Failed save
+      flash[:error] = 'Failed to create like'
     end
   end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new( author_id_id: params[:user_id], title: params[:post][:title],
+    @post = Post.new(author_id_id: params[:user_id], title: params[:post][:title],
                      text: params[:post][:text], comments_counter: 0, likes_counter: 0)
     if @post.save
       # Successful save
       redirect_to "/users/#{params[:user_id]}/posts/#{@post.id}"
     else
       # Failed save
-      flash[:error] = "Failed to create like"
+      flash[:error] = 'Failed to create like'
     end
   end
 end
