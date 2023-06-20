c08bc5661eaffa4f807575289add06e9770374b5
shayan1234554321 <kshayan091@gmail.com>
2023-06-20 22:45:03 +0500

    added tests

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'User Index Page', type: :feature do
-  let(:first_user) { User.create(name: 'John', photo: 'img-one.jpg', bio: 'Artist from Kenya.') }
-  let(:second_user) { User.create(name: 'Shayan', photo: 'img-two.jpg', bio: 'Artist from Pakistan.') }
-
+  
   before do
-    Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-    Post.create(author: second_user, title: 'World of tech', text: 'This is my second post')
-
+    @first_user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Artist from Kenya.') 
+    @second_user = User.create(name: 'Shayan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Artist from Pakistan.')
+    Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+    Post.create(author: @second_user, title: 'World of tech', text: 'This is my second post')
     visit users_path
   end
 
@@ -21,15 +20,15 @@ RSpec.describe 'User Index Page', type: :feature do
   end
 
   it 'Displays the number of posts each user has written.' do
-    expect(page).to have_content(first_user.posts.count)
-    expect(page).to have_content(second_user.posts.count)
+    expect(page).to have_content(@first_user.posts_counter)
+    expect(page).to have_content(@second_user.posts_counter)
   end
 
   it 'redirects to user show page when clicking on a user' do
-    visit user_path(first_user)
-    expect(page).to have_current_path(user_path(first_user))
+    visit user_path(@first_user)
+    expect(page).to have_current_path(user_path(@first_user))
 
-    visit user_path(second_user)
-    expect(page).to have_current_path(user_path(second_user))
+    visit user_path(@second_user)
+    expect(page).to have_current_path(user_path(@second_user))
   end
 end
--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User show page', type: :feature do
-  let!(:user) { User.create(name: 'John', photo: 'img-one.jpg', bio: 'Artist from Kenya.') }
+  let!(:user) { User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Artist from Kenya.') }
   let!(:first_post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
   let!(:second_post) { Post.create(author: user, title: 'World of tech', text: 'This is my second post') }
   let!(:third_post) { Post.create(author: user, title: 'Coding is fun', text: 'This is my third post') }
@@ -33,27 +33,20 @@ RSpec.describe 'User show page', type: :feature do
   end
 
   it 'Shows a button that lets me view all of a users posts.' do
-    expect(page).to have_link('See all posts', href: user_posts_path(user))
-  end
+    click_on 'See all posts'
+    expect(page).to have_content('This is my first post')
+    expect(page).to have_content('This is my second post')
+    expect(page).to have_content('This is my third post')
+end
 
-  it 'redirects to the post show page when clicking on a post' do
-    click_link 'Hello'
-    expect(page).to have_current_path(post_path(first_post))
-
-    # Go back to the user show page
-    visit user_path(user)
-
-    click_link 'World of tech'
-    expect(page).to have_current_path(post_path(second_post))
-
-    visit user_path(user)
-
-    click_link 'coding is fun'
-    expect(page).to have_current_path(post_path(third_post))
+it 'redirects to the post show page when clicking on a post' do
+    click_on first_post.title
+    expect(page).to have_content('This is my first post')
   end
 
   it 'redirects to the user posts index page when clicking "View All Posts"' do
-    click_link 'See all posts'
-    expect(page).to have_current_path(user_posts_path(user))
+    visit user_post_path(user.id , first_post.id)
+    click_link 'Back to all the posts'
+    expect(page).to have_current_path(user_posts_path(user.id))
   end
 end
