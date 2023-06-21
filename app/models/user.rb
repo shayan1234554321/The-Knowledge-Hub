ceac49f878ad42df006ce9a14e4ab8691b4db6d1
shayan1234554321 <kshayan091@gmail.com>
2023-06-22 01:01:19 +0500

    Added letter opner and added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
+  attr_accessor :confirmation_sent_at
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_many :posts, foreign_key: 'author_id_id', class_name: 'Post'
