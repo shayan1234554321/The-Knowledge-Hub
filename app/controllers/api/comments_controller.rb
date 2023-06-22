451721f0b386813d77b75470f3e371904805f042
johnie261 <njorogejohn261@gmail.com>
2023-06-22 19:16:58 +0300

    Fix errors in controllers/api files

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,22 +5,22 @@ class Api::CommentsController < ActionController::Base
   end
 
   def create
-    message = "Invalid credentials"
+    message = 'Invalid credentials'
     @user = User.find_by_id(params[:user_id])
     if @user.present?
-        @post = Post.find_by_id(params[:post_id])
-        if @post.present?
-            @comments = @comment = Comment.new(author: @user , post: @post , text: params[:text])
-            if @comment.save
-                message = "Comment created"
-            else
-                message = "There was an error"
-            end
-        else
-          message = "No Post available"
-        end
+      @post = Post.find_by_id(params[:post_id])
+      if @post.present?
+        @comments = @comment = Comment.new(author: @user, post: @post, text: params[:text])
+        message = if @comment.save
+                    'Comment created'
+                  else
+                    'There was an error'
+                  end
+      else
+        message = 'No Post available'
+      end
     else
-      message = "No User avilable"
+      message = 'No User avilable'
     end
 
     render json: message
